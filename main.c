14117fb2fe1eac046c0570ef6f80ed8fad92d257
Luke LaPlante <lelaplante417@gmail.com>
2023-06-05 17:22:14 -0400

    Fixing the spelling of "Circumference"

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 
 int main() {
      const double PI = 3.141593;
-     printf("WELCOME TO CIRCUMFRENCE CALCULATOR!!!\nPlease input the radius of your circle:\n> ");
+     printf("WELCOME TO CIRCUMFERENCE CALCULATOR!!!\nPlease input the radius of your circle:\n> ");
      double radius;
      scanf("%lf", &radius);
      double out = 2 * PI * radius;
-     printf("The circumfrence of your circle is %lf.\nTo exit, type anything and press enter.\n", out);
+     printf("The circumference of your circle is %lf.\nTo exit, type anything and press enter.\n", out);
      scanf("%d");
      return 0;
 }
